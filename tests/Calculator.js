4d452857eaa4b6f7d65ab55c28efe99ea680a6b7
jjgamestudio <qasimjaved13@gmail.com>
2021-03-19 19:17:16 +0500

    Calculator.js updated

--- tests/Calculator.js
+++ tests/Calculator.js
@@ -21,23 +21,4 @@ describe('Demo Calculator', function() {
         browser.sleep(3000);
     });
 
-    it('should Subtraction test', function () {
-        //browser.get('https://juliemr.github.io/protractor-demo/');
-        homepage.get('https://juliemr.github.io/protractor-demo/');
-
-        //element(by.model('first')).sendKeys('2');
-        homepage.enterfirstnumber('4');
-
-        //element(by.model('second')).sendKeys('3');
-        homepage.entersecondnumber('6');
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickbutttono1();
-
-        //let result=element(by.cssContainingText('.ng-binding','5'));
-        //expect(result.getText()).toEqual('5');
-        homepage.verify('10');
-
-        browser.sleep(4000);
-    });
 });
